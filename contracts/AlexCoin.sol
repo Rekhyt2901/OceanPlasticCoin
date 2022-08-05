b99bae7af52b9d2ee1acebd250dbeea47c155537
Rekhyt2901 <alex@deminator.de>
2022-08-05 18:02:02 +0200

    added transfer, transferfrom, approve

--- contracts/AlexCoin.sol
+++ contracts/AlexCoin.sol
@@ -3,20 +3,58 @@ pragma solidity ^0.8.15;
 
 contract AlexCoin {
     uint256 public totalSupply;
+    string public name;
+    string public symbol;
+    string public standard;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor() {
         totalSupply = 86000000000;
+        name = "AlexCoin";
+        symbol = "ALEX";
+        standard = "Alex Coin v1.0";
+        balanceOf[msg.sender] = totalSupply;
+
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 
-    /* function name() public view returns (string)
-    function symbol() public view returns (string)
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
+        require(allowance[_from][msg.sender] >= _value);
+        require(balanceOf[_from] >= _value);
+
+        allowance[_from][msg.sender] -= _value;
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /* 
+        NOT DONE
     function decimals() public view returns (uint8)
-    function totalSupply() public view returns (uint256)
-    function balanceOf(address _owner) public view returns (uint256 balance)
-    function transfer(address _to, uint256 _value) public returns (bool success)
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-    function approve(address _spender, uint256 _value) public returns (bool success)
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining)
 
     function _transfer(address sender, address recipient,uint256 amount) internal returns (bool success)
     function _mint(address account, uint256 amount) internal returns (bool success)
@@ -24,8 +62,20 @@ contract AlexCoin {
     function _approve(address owner, address spender, uint256 amount) internal returns (bool success)
     function _burnFrom(address account, uint256 amount) internal returns (bool success)
     
+
+        DONE
+    function name() public view returns (string)
+    function symbol() public view returns (string)
+    function totalSupply() public view returns (uint256)
+    function balanceOf(address _owner) public view returns (uint256 balance)
+    function transfer(address _to, uint256 _value) public returns (bool success)
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
+    function approve(address _spender, uint256 _value) public returns (bool success)
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+
     event Transfer(address indexed _from, address indexed _to, uint256 _value)
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value) */
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+    */
 
 
 }
